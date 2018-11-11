b4f981fae5d36a975d07ad67896899701a774f20
Nail` Khalimov <nail980@mail.ru>
2018-11-11 15:18:38 +0300

    added main components

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import NewsSlider from '../widgets/NewsSlider/slider';
+import NewsList from '../widgets/NewsList/newsList';
+import VideoList from '../widgets/VideosList/VideoList';
+
 const Home = () => {
   return (
     <div>
-      Home
+      <NewsSlider
+        type='featured'
+        start={0}
+        amount={3}
+        settings={{
+          dots: false
+        }}
+      />
+      <NewsList
+        type="card"
+        loadmore={true}
+        start={3}
+        amount={3}
+      />
+      <VideoList
+        type="card"
+        title={true}
+        loadmore={true}
+        start={0}
+        amount={3}
+      />
     </div>
   )
 }
